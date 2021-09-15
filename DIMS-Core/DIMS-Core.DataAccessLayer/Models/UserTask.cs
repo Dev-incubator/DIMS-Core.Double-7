469a5797f3cdf774e2d648a9dd1f1367e3b1dc5f
Ivan <67104943+kuzmiich@users.noreply.github.com>
2021-09-15 23:28:04 +0300

    Scaffold models from database (#4)
    * Create table script tasks
    create procedure to delete task
    create views to task & userTrack
    
    * Add TaskTrack, UserTask,TaskState tables.
    Add vUserTask view.
    Add stored procedures SetUserTaskAsFail, SetUserTaskAsSuccess.
    Add SeedData for TaskState.
    
    * Deleted file DIMS-Core.Database.refactorlog
    Edited procedure DeleteTask.sql
    Rename vTask.sql to vTasks.sql
    
    * Renamed Task to Tasks
    
    * tooked out primary key in Tasks.sql
    renamed Task to Tasks
    
    * fix sql scripts
    
    * Fix non plural names in stored procedures.
    Fix column name StartDate in table Tasks(were StateDate).
    Fix non plural names in view VUserTask.
    Fix join relationship in view vUserTrack.
    
    * Scaffold models from database
    
    * Updated efcore to 5.0.9
    Create tables in database, and rescaffold models from Database
    
    * Transferred files from the Core to DataAccessLayer
    
    * Transferred Task.cs file & refactor
    
    * Added Task.cd, VTask.cs, VUserTrack.cs, VUserTrack.cs & updated context
    
    * Fixed comments
    
    Co-authored-by: Арсений <ars.stank@gmail.com>
    Co-authored-by: Konstantin Borovik <kes8594@gmail.com>
    Co-authored-by: aces47 <35996769+aces47@users.noreply.github.com>

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Models/UserTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DIMS_Core.DataAccessLayer.Models
+{
+    public partial class UserTask
+    {
+        public int UserTaskId { get; set; }
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public int StateId { get; set; }
+
+        public virtual TaskState State { get; set; }
+        public virtual TaskTrack Task { get; set; }
+        public virtual UserProfile User { get; set; }
+        public virtual Task UserTaskNavigation { get; set; }
+    }
+}
