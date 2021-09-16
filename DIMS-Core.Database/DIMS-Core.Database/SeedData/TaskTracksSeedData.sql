1b7cfb10c1f6377d881d60639a1aa81ed61e5aed
Ivan <ivan.kuzmiich@gmail.com>
2021-09-16 18:31:33 +0300

    Added TasksSeedData.sql, TaskTracksSeedData.sql Fixed name to TaskState.sql, TaskStateSeedData.sql Updated sqlproj file

---
+++ DIMS-Core.Database/DIMS-Core.Database/SeedData/TaskTracksSeedData.sql
@@ -0,0 +1,14 @@
+INSERT INTO [dbo].[TaskTracks] 
+    (
+        UserTaskId, TrackDate, TrackNote
+    ) 
+    VALUES 
+    (
+        1, '2021-06-13 6:00:00', 'Some note'
+    ),
+    (
+        2, '2021-06-11 15:30:00', 'Some note 2'
+    ),
+    (
+        3, '2021-06-11 17:25:20', 'Some note 3'
+    )
