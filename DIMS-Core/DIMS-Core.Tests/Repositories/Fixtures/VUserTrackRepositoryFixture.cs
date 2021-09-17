fa1c4b0b5050cc3374531531c781a39a64fc7c02
Ivan <ivan.kuzmiich@gmail.com>
2021-09-17 17:23:00 +0300

    Added Fixture repositories

---
+++ DIMS-Core/DIMS-Core.Tests/Repositories/Fixtures/VUserTrackRepositoryFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DIMS_Core.DataAccessLayer.Models;
+using DIMS_Core.DataAccessLayer.Repositories;
+using DIMS_Core.Tests.Repositories.Fixtures.Base;
+using Microsoft.EntityFrameworkCore;
+using Task = System.Threading.Tasks.Task;
+
+namespace DIMS_Core.Tests.Repositories.Fixtures
+{
+    public class VUserTrackRepositoryFixture : BaseRepositoryFixture<VUserTrackRepository>, IDisposable
+    {
+        protected override VUserTrackRepository CreateRepository() => new(Context);
+
+        protected override async Task InitDatabase()
+        {
+            var entity = await Context.VUserTracks.AddAsync(new VUserTrack
+                                              {
+                                                  UserId = 1,
+                                                  TaskId = 1,
+                                                  TaskTrackId = 1,
+                                                  TaskName = "Name",
+                                                  TrackNote = "Note",
+                                                  TrackDate = new DateTime(2021, 02, 03, 08, 20, 50)
+                                              });
+            
+            await Context.SaveChangesAsync();
+            entity.State = EntityState.Detached;
+        }
+
+        public void Dispose() => Context.Dispose();
+
+    }
+}
