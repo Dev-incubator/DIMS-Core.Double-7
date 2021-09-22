2106d0432b77335c8760a64e59fca11eeb23dde8
Арсений <ars.stank@gmail.com>
2021-09-22 20:10:13 +0300

    make vUserTaskRepository

--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserTaskRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/VUserTaskRepository.cs
@@ -1,12 +1,18 @@
+using DIMS_Core.DataAccessLayer.Interfaces;
 using DIMS_Core.DataAccessLayer.Models;
-using DIMS_Core.DataAccessLayer.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DIMS_Core.DataAccessLayer.Repositories
 {
-    public class VUserTaskRepository : ReadOnlyRepository<VUserTask>
+    public class vUserTaskRepository : ReadOnlyRepository<VUserTask>
     {
 
-        public VUserTaskRepository(DimsCoreContext context) : base(context)
+        public vUserTaskRepository(DimsCoreContext context) : base(context)
         {
 
         }
