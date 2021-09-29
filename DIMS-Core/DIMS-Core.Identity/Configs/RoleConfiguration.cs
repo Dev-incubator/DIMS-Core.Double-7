2eddcf7426982dca5ae1715c690e24b8df7b5505
Konstantin Borovik <kes8594@yandex.ru>
2021-09-29 21:40:58 +0300

    - add relationship between user and role - add admin account

--- DIMS-Core/DIMS-Core.Identity/Configs/RoleConfiguration.cs
+++ DIMS-Core/DIMS-Core.Identity/Configs/RoleConfiguration.cs
@@ -1,25 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using DIMS_Core.Identity.Entities;
+﻿using DIMS_Core.Identity.Entities;
+using DIMS_Core.Identity.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DIMS_Core.Identity.Configs
 {
-    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    internal class RoleConfiguration : IEntityTypeConfiguration<Role>
     {
         public void Configure(EntityTypeBuilder<Role> builder)
         {
-            builder.HasData(GetRolesData());
-        }
-
-        private static IEnumerable<Role> GetRolesData()
-        {
-            return IdentityConstants.RoleNames.Roles.Select((t, i) => new Role
-                                                                      {
-                                                                          Id = i + 1,
-                                                                          Name = t
-                                                                      });
+            builder.HasMany(q => q.UserRoles)
+                   .WithOne()
+                   .HasForeignKey(q => q.RoleId);
+            builder.HasData(RoleHelper.GetRolesData());
         }
     }
 }
---
+++ DIMS-Core/DIMS-Core.Identity/Migrations/20210929183719_AddRelationsUserRolesAndAdmin.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DIMS_Core.Identity.Migrations
+{
+    public partial class AddRelationsUserRolesAndAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "0e147196-2abd-4c8d-9096-3dbdd17a7f49");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ac4ccdef-95c3-4453-b865-a703ecd02ae6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "7ac7757a-f6bc-41f9-84a1-78e6155efd21");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { 1, 0, "af764071-18f5-4a82-b94a-0c1d49b36176", "admin@test.com", false, false, null, "ADMIN@TEST.COM", "ADMIN@TEST.COM", "AQAAAAEAACcQAAAAEOt343biP2ixRzbwJYTtP/SffcFo8mJgX9Gh+1uwiiPgIiymkaVoproeh9RBGkk+0w==", null, false, "WGOYLD7Z2HHLP53GU356Q7AO2TOPH3BY", false, "admin@test.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { 3, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "425b0a6a-71aa-4523-b6bb-edc6a8b9cddc");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "e7c1ba3e-771d-4ba2-9620-4a58c1ee7da6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "e8546d82-8f96-4fad-8f6e-e176087ed111");
+        }
+    }
+}
